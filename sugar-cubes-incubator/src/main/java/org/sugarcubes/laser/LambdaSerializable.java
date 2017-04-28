adfc120aae6f44a64423f9c1f482720af1d4a563
Maxim Butov <mbutov@qulix.com>
2017-04-29 00:46:03 +0300

    Laser

--- sugar-cubes-incubator/src/main/java/org/sugarcubes/laser/LambdaSerializable.java
+++ sugar-cubes-incubator/src/main/java/org/sugarcubes/laser/LambdaSerializable.java
@@ -1,6 +1,7 @@
 package org.sugarcubes.laser;
 
 import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
 
 /**
  * A serializable form of a lambda.
@@ -10,11 +11,23 @@ import java.io.Serializable;
  */
 public class LambdaSerializable implements Serializable {
 
+    private Object lambda;
+
     public LambdaSerializable(Object lambda) {
+        SerializedLambda serializedLambda = new SerializedLambda(LambdaSerializable.class,
+            "", "", "",
+            0, "", "", "",
+            "", new Object[] {this,});
+        lambda = serializedLambda;
     }
 
     public Object toLambda() {
-        return null;
+        return lambda;
+    }
+
+    private static Object $deserializeLambda$(SerializedLambda serializedLambda) {
+        LambdaSerializable lambdaSerializable = (LambdaSerializable) serializedLambda.getCapturedArg(0);
+        return (Runnable) () -> {};
     }
 
 }
