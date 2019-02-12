2a3a310bd77626d335c5f5af4abd30602ee1a449
Maxim Butov <mbutov@qulix.com>
2019-02-12 11:59:32 +0300

    primitives

--- sugar-cubes-sandbox/src/main/java/org/sugarcubes/primitive/XPrimitiveType.java
+++ sugar-cubes-sandbox/src/main/java/org/sugarcubes/primitive/XPrimitiveType.java
@@ -10,53 +10,57 @@ import org.sugarcubes.function.TernaryConsumer;
  */
 public class XPrimitiveType<W, A> {
 
+    private static final Function UNSUPPORTED = x -> {
+        throw new UnsupportedOperationException();
+    };
+
     public static final XPrimitiveType<Boolean, boolean[]> BOOLEAN = new XPrimitiveType<>(
         array -> array.length,
         boolean[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, UNSUPPORTED, UNSUPPORTED);
 
     public static final XPrimitiveType<Byte, byte[]> BYTE = new XPrimitiveType<>(
         array -> array.length,
         byte[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::byteValue);
 
     public static final XPrimitiveType<Short, short[]> SHORT = new XPrimitiveType<>(
         array -> array.length,
         short[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::shortValue);
 
     public static final XPrimitiveType<Character, char[]> CHARACTER = new XPrimitiveType<>(
         array -> array.length,
         char[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, c -> (int) c, number -> (char) number.intValue());
 
     public static final XPrimitiveType<Integer, int[]> INTEGER = new XPrimitiveType<>(
         array -> array.length,
         int[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::intValue);
 
     public static final XPrimitiveType<Long, long[]> LONG = new XPrimitiveType<>(
         array -> array.length,
         long[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::longValue);
 
     public static final XPrimitiveType<Float, float[]> FLOAT = new XPrimitiveType<>(
         array -> array.length,
         float[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::floatValue);
 
     public static final XPrimitiveType<Double, double[]> DOUBLE = new XPrimitiveType<>(
         array -> array.length,
         double[]::new,
         (array, index) -> array[index],
-        (array, index, value) -> array[index] = value);
+        (array, index, value) -> array[index] = value, number -> number, Number::doubleValue);
 
     private final Function<A, Integer> arrayLength;
     private final Function<Integer, A> arrayFactory;
@@ -68,12 +72,17 @@ public class XPrimitiveType<W, A> {
     private final Class<W> wrapperType;
     private final W defaultValue;
 
-    public XPrimitiveType(Function<A, Integer> arrayLength, Function<Integer, A> arrayFactory, BiFunction<A, Integer, W> arrayGet, TernaryConsumer<A, Integer, W> arraySet) {
+    private final Function<W, Number> toNumber;
+    private final Function<Number, W> fromNumber;
+
+    public XPrimitiveType(Function<A, Integer> arrayLength, Function<Integer, A> arrayFactory, BiFunction<A, Integer, W> arrayGet, TernaryConsumer<A, Integer, W> arraySet, Function<W, Number> toNumber, Function<Number, W> fromNumber) {
 
         this.arrayLength = arrayLength;
         this.arrayFactory = arrayFactory;
         this.arrayGet = arrayGet;
         this.arraySet = arraySet;
+        this.toNumber = toNumber;
+        this.fromNumber = fromNumber;
 
         A array = arrayFactory.apply(1);
 
@@ -113,13 +122,6 @@ public class XPrimitiveType<W, A> {
 
     public void set(A array, int index, W value) {
         arraySet.accept(array, index, value);
-    }
-
-    public static void main(String[] args) {
-        long x = Long.MAX_VALUE;
-        float y = x;
-        long z = (long) y;
-        System.out.println(x==z);
     }
 
 }
