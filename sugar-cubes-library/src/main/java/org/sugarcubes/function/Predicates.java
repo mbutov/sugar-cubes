4584dc9e53f32ee0a8312f2f328289dfb1494a24
Maxim Butov <mbutov@qulix.com>
2019-06-16 23:25:14 +0300

    Refactoring

--- sugar-cubes-library/src/main/java/org/sugarcubes/function/Predicates.java
+++ sugar-cubes-library/src/main/java/org/sugarcubes/function/Predicates.java
@@ -1,7 +1,8 @@
 package org.sugarcubes.function;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
+import static java.util.stream.Stream.of;
 
 /**
  * todo: document it
@@ -15,11 +16,15 @@ public class Predicates {
     }
 
     public static <T> Predicate<T> and(Predicate<T> first, Predicate<T> second, Predicate<T>... rest) {
-        return Stream.of(rest).reduce(first.and(second), Predicate::and);
+        return reduce(Predicate::and, first, second, rest);
     }
 
     public static <T> Predicate<T> or(Predicate<T> first, Predicate<T> second, Predicate<T>... rest) {
-        return Stream.of(rest).reduce(first.or(second), Predicate::or);
+        return reduce(Predicate::or, first, second, rest);
+    }
+
+    private static <X> X reduce(BinaryOperator<X> accumulator, X first, X second, X... rest) {
+        return of(rest).reduce(accumulator.apply(first, second), accumulator);
     }
 
     public static <T> Predicate<T> xor(Predicate<T> first, Predicate<T> second) {
